0cf10a4e454baf20d972a5f5bac609016bcb56ef
James Hao <mhao1999@live.cn>
2016-02-25 22:09:44 +0800

    set mailsender

--- src/main/java/com/goodx/configuration/GoodXToolConfig.java
+++ src/main/java/com/goodx/configuration/GoodXToolConfig.java
@@ -1,11 +1,14 @@
 package com.goodx.configuration;
 
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 
 @Configuration
 @PropertySource(value="classpath:application.properties")
@@ -22,7 +25,14 @@ public class GoodXToolConfig {
 		sender.setPort(environment.getProperty("mail.port", Integer.class));
 		sender.setUsername(environment.getProperty("mail.username"));
 		sender.setPassword(environment.getProperty("mail.password"));
-		sender.setProtocol(environment.getProperty("mail.protocol"));
+
+		Properties javaMailProperties = new Properties();
+		javaMailProperties.setProperty("mail.transport.protocol", environment.getProperty("mail.protocol"));
+		javaMailProperties.setProperty("mail.smtp.auth", environment.getProperty("mail.auth"));
+		javaMailProperties.setProperty("mail.smtp.starttls.enable", environment.getProperty("mail.starttls"));
+		javaMailProperties.setProperty("mail.debug", environment.getProperty("mail.debug"));
+		
+		sender.setJavaMailProperties(javaMailProperties);
 		
 		return sender;
 	}
